d1c6eb108203fc7bda5f4a2752fcfe851762c92c
Rob <coolwop@gmail.com>
2014-11-21 15:03:29 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,36 +4,37 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-        inv<-matrix()
-        solved<-FALSE
-        set <- function(y) {
-                x <<- y
-                inv<<-matrix()
-        }
-        get <- function() x
-        setinv <- function(inv_mat) {
-                solved<<-TRUE
-                inv <<- inv_mat
-        }
-        issolved <- function() solved   
-        getinv <- function() inv
-        list(set = set, get = get,
-             setinv = setinv, issolved=issolved,
-             getinv = getinv)
+  inv<-matrix()
+  solved<-FALSE
+  set <- function(y) {
+    x <<- y
+    inv<<-matrix()
+    solved<<-FALSE
+  }
+  get <- function() x
+  setinv <- function(inv_mat) {
+    solved<<-TRUE
+    inv <<- inv_mat
+  }
+  issolved <- function() solved   
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv, issolved=issolved,
+       getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve<-function(x,...) {
-        inv <- x$getinv()
-        data <- x$get()
-        if( identical(x$get(),data) & x$issolved() )  {
-                message("getting cached data")
-                return(inv)
-        }
-        
-        inv <- solve(data,...)
-        x$setinv(inv)
-        inv
+  inv <- x$getinv()
+  data <- x$get()
+  if( identical(x$get(),data) & x$issolved() )  {
+    message("getting cached data")
+    return(inv)
+  }
+  
+  inv <- solve(data,...)
+  x$setinv(inv)
+  inv
 }
